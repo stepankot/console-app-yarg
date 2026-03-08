5a0d4d8117af9796199ec837f6c78f6cb1d5bdac
STAP16 <stepancat61@gmail.com>
2026-03-08 11:39:44 +0300

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+const yargs = require('yargs/yargs');
+const {addNote, printNotes, deleteNote} = require('./notes.controller')
+const { hideBin } = require('yargs/helpers');
+
+
+const yarg = hideBin(process.argv);
+
+
+yargs(yarg)
+  .command('add', 'Add new note', (yargs) => {
+		return yargs.option('title', {
+			type: 'string',
+			describe: 'Название заметки',
+			demandOption: true,
+		})
+	}, ({title}) => {
+    addNote(title)
+  })
+  .command('remove', 'Remove note by id', (yargs) => {
+		return yargs.option('id', {
+			type: 'string',
+			describe: 'ID заметки',
+			demandOption: true,
+		})
+	}, ({id}) => {
+    deleteNote(id)
+  })
+  .command('list', 'List all notes', () => {}, async () => {
+    const notes = await printNotes()
+		console.log(notes)
+  })
+  .demandCommand(1, 'Укажите команду')
+  .help()
+  .parse()
+  
+
+yargs(yarg).version('1.0.0')
---
+++ notes.controller.js
@@ -0,0 +1,50 @@
+const fs = require('fs/promises')
+const path = require('path')
+const chalk = require('chalk')
+
+const notesPath = path.join(__dirname, 'db.json')
+const log = console.log;
+
+async function writeFileNotes(notes) {
+	await fs.writeFile(notesPath, JSON.stringify(notes))
+}
+
+
+async function addNote(title) {
+	const notes = await getNotes()
+	const note = {
+		id: Date.now().toString(),
+		title,
+	}
+	notes.push(note)
+	await writeFileNotes(notes)
+	log(chalk.green("Заметка добавлена!"))
+}
+
+async function getNotes() {
+	const notes = await fs.readFile(notesPath, {encoding: 'utf-8'})
+	const parseNotes = JSON.parse(notes)
+	return Array.isArray(parseNotes) ? parseNotes : []
+}
+
+async function deleteNote(id) {
+	const notes = await getNotes();
+	const note = notes.find(note => note.id === id)
+	
+	if (!note) return log(chalk.red("Заметки с id:", id, "не существует"))
+	const newNotesArray = notes.filter(note => note.id !== id)
+	await writeFileNotes(newNotesArray)
+	log(chalk.green(`Заметка ${note.title} успешно удалена!`))
+}
+
+async function printNotes() {
+	const notes = await getNotes()
+	log(chalk.bgBlue("Here is the list of notes:"))
+	console.table(notes, ['title', 'id']);
+}
+
+module.exports = {
+	addNote,
+	printNotes,
+	deleteNote
+}
